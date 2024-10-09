34443c3a651f2217970fb7006bf54f9e11eeac76
IlhamLamp <ilhamutomo24@gmail.com>
2024-10-09 20:26:33 +0700

    fix: logout with jwt bearer token ;

--- src/controllers/logout.controller.ts
+++ src/controllers/logout.controller.ts
@@ -1,19 +1,36 @@
 import express from 'express';
 import redisClient from '../config/redis_client';
+import jwt from 'jsonwebtoken';
 
 export const Logout = async(req: express.Request, res: express.Response) => {
-    const { id } = req.body;
-    if (!id) {
-        res.status(400).json({ message: 'Invalid ID.', status: 400})
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        res.status(401).json({ message: 'Missing authorization headr', status: 401 })
         return;
     }
-    const uid = id.toString();
+    const token = authHeader.split(' ')[1];
+    if (!token) {
+        res.status(401).json({ message: 'Invalid token', status: 401 });
+        return;
+    }
+    try {
+        const accessToken = process.env.JWT_ACCESS_SECRET;
+        if (!accessToken) {
+            throw new Error('JWT_ACCESS_SECRET is not defined');
+        }
+        const decodedToken: any = jwt.verify(token, accessToken);
+        const uid = String(decodedToken.id);
 
-    const userExists = await redisClient.exists(uid);
-    if (!userExists) {
-        res.status(404).json({ message: 'User not found in Redis.', status: 404 });
+        const userExists = await redisClient.exists(uid);
+        if (!userExists) {
+            res.status(404).json({ message: 'User not found in Redis.', status: 404 });
+            return;
+        }
+        await redisClient.del(uid);
+        res.status(200).json({ message: 'Logged out successfully', status: 200});
+        return;
+    } catch (error) {
+        res.status(401).json({ message: 'Invalid token', status: 401 });
         return;
     }
-    await redisClient.del(uid);
-    res.status(200).json({ message: 'Logged out successfully', status: 200})
 }
