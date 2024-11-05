21a9af5a2f2459a761ab71000b1ebbf8a417a95b
IlhamLamp <ilhamutomo24@gmail.com>
2024-11-05 23:15:05 +0700

    reconnecting to redis

---
+++ src/middleware/check_conn_redis.ts
@@ -0,0 +1,7 @@
+// const ensureRedisConnection = async () => {
+//   if (!redisClient.isOpen) {
+//     console.log("Reconnecting to Redis...");
+//     await redisClient.connect();
+//   }
+//   resetIdleTimer();
+// };
