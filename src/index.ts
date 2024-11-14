6164f7d309382efb4b222fbaa899e24b91d94bea
IlhamLamp <ilhamutomo24@gmail.com>
2024-11-14 21:44:30 +0700

    test: ci.yml

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1/auth", router);
 
+// root
 app.get("/", (req: express.Request, res: express.Response) => {
   res.status(200).send({
     response: "Express TypeScript",
