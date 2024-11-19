e49e2308095b5724daaa6617440c42c3f5ed0df5
IlhamLamp <ilhamutomo24@gmail.com>
2024-11-20 00:32:53 +0700

    add: oauth google callback env

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -7,4 +7,5 @@ export const APP_NAME = process.env.APP_NAME ?? "Communtity Authentication";
 export const APP_CLIENT = process.env.APP_CLIENT ?? "http://localhost:3000";
 
 export const AuthGoogleCallbackURL =
+  process.env.GOOGLE_REDIRECT_URI ??
   "http://localhost:3001/api/v1/auth/google/callback";
