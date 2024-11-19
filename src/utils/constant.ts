d560cafe569ba97a1dbbb459d2a6207c8d89f625
IlhamLamp <ilhamutomo24@gmail.com>
2024-11-19 23:41:15 +0700

    tst

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 
 dotenv.config();
-//
+
 export const PORT = process.env.APP_PORT ?? 3001;
 export const APP_NAME = process.env.APP_NAME ?? "Communtity Authentication";
 export const APP_CLIENT = process.env.APP_CLIENT ?? "http://localhost:3000";
