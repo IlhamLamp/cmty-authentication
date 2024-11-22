cfd0ecee4823e40ed4fc5ce4b7b449931d8ac199
IlhamLamp <ilhamutomo24@gmail.com>
2024-11-22 13:02:52 +0700

    fix: auth-runner

--- src/index.ts
+++ src/index.ts
@@ -23,6 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use("/api/v1/auth", router);
 
+// THIS LINE IS DUMMY COMMIT FOR TEST WORKFLOWS
 app.get("/", (req: express.Request, res: express.Response) => {
   res.status(200).send({
     response: "Express TypeScript",
